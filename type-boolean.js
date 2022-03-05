114c7fb547664e4cf4da49aaf32c280005ce2abc
Kalyell <kalyellventura@gmail.com>
2022-03-05 20:36:30 -0300

    todos os arquivos

---
+++ type-boolean.js
@@ -0,0 +1,15 @@
+// boolean
+// true => verdadeiro
+// false => falso
+
+const primeiroNumenro = 5;
+const segundoNumero = 5;
+let cadastroativado = true;
+
+//console.log(primeiroNumenro === segundoNumero);
+
+const texto1 = "Alura"
+const texto2 = "alura"
+
+console.log(texto1 === texto2)
+
---
+++ type-number.js
@@ -0,0 +1,24 @@
+// tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumento = 2;
+
+const operacaoMatematica = primeiroNumero - segundoNumento;
+
+console.log(operacaoMatematica)
+
+// ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao)
+
+// NaN -> Not A Number (mão é um número)
+
+const alura = "Alura";
+console.log(alura * primeiroNumero)
